4b1c01dc6e5fd7192aa6aad0be00cf8a01ba3bf5
Christopher Arnold <ChristopherArnold@namitentou.com>
2019-01-26 13:28:01 -0800

    TitleScreen

---
+++ Assets/Scripts/ButtonLoadScene.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonLoadScene : MonoBehaviour
+{
+    public string scene;
+    private Button button;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(OnClick);
+    }
+
+    private void OnClick()
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
