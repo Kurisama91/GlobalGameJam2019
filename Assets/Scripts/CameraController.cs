4e0c154923817de7f0c22518b056911ee23ab896
Christopher Arnold <ChristopherArnold@namitentou.com>
2019-01-26 13:33:42 -0800

    Follow Camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject focusingObject;
+    public float followSpeed = 10f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Vector3 focusingObjectPos = focusingObject.transform.position;
+        focusingObjectPos.z = transform.position.z;
+        transform.position = focusingObjectPos;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector3 focusingObjectPos = focusingObject.transform.position;
+        focusingObjectPos.z = transform.position.z;
+        transform.position = Vector3.Lerp(transform.position, focusingObjectPos, followSpeed * Time.deltaTime);
+    }
+}
